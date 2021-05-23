d94ecf5b8ee0e0eb0dfef96fcabafce4dbfa3cb2
eldersquid <pristinpinky@gmail.com>
2021-05-23 21:07:26 +0800

    4th commit

--- routes/admin.js
+++ routes/admin.js
@@ -12,9 +12,35 @@ router.get('/hospitalList', (req, res) => {
 });
 
 router.get('/hospitalSearch', (req, res) => {
+	const title = "Search Hospital";
 	res.render('admin/hospital/hospital_search', { layout : "admin",
                                         title : title});
 	console.log(req.body.hospital);
+
+});
+
+router.get('/hospitalCreate', (req, res) => {
+	const title = "Create Hospital";
+	res.render('admin/hospital/hospital_create', { layout : "admin",
+                                        title : title});
+	
+
+});
+
+router.get('/VehicleList', (req, res) => {
+	const title = 'Vehicles';
+	
+	res.render('admin/vehicles/vehicle_list', { layout : "admin",
+                                        title : title}); 
+
+});
+
+
+
+router.get('/hospitalCreated', (req, res) => {
+	res.render('admin/hospital/hospital_list', 
+	{ layout : "admin"});
+	
 
 });
 
