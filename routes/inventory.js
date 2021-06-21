0baa1040535b5f75877643f1c8872049aa636283
Wong Zi Yuan <ziyuan2497@gmail.com>
2021-06-22 00:28:27 +0800

    ziyuan

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const Inventory = require('../models/Inventory');
+const alertMessage = require('../helpers/messenger.js');
+
+// router.get('/view', (req, res) => {
+//     const title = 'Inventory';
+
+//     res.render('inventory/view', {
+//         layout: "admin",
+//         title: title
+//     });
+
+// });
 
 router.get('/view', (req, res) => {
-    const title = 'Inventory';
-
-    res.render('inventory/view', {
-        layout: "admin",
-        title: title
-    });
-
+    Inventory.findAll({
+        where: {
+            adminId: req.admin.id
+        },
+        order: [
+            ['item_name', 'ASC']
+        ],
+        raw: true
+    })
+        .then((inventory) => {
+            // pass object to listInventory.handlebar
+            res.render('inventory/view', {
+                inventory: inventory
+            });
+        })
+        .catch(err => console.log(err));
 });
 
-router.get('/create', (req, res) => {
+router.get('/showCreate', (req, res) => {
     const title = 'Inventory';
 
     res.render('inventory/create', {
@@ -20,12 +41,101 @@ router.get('/create', (req, res) => {
     });
 });
 
+router.post('/create', (req, res) => {
+    let item_name = req.body.item_name;
+    let supplier = req.body.supplier;
+    let product_name = req.body.product_name;
+    let quantity = req.body.quantity;
+
+    Inventory.create({
+        item_name,
+        supplier,
+        product_name,
+        quantity
+    }).then((inventory) => {
+        res.redirect('/inventory/view');
+    }).catch(err => console.log(err))
+});
+
 router.get('/update', (req, res) => {
     const title = 'Inventory';
 
     res.render('inventory/update', {
         layout: "admin",
         title: title
+    });
+});
+
+router.get('/showUpdate/:id', (req, res) => {
+    Inventory.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then((inventory) => {
+        if (!inventory) { // check inventory first because it could be null.
+            alertMessage(res, 'info', 'No such Inventory', 'fas fa-exclamation-circle', true);
+            res.redirect('/inventory/view');
+        } else {
+            // Only authorised admin who is owner of inventory can edit it
+            if (req.admin.id === inventory.adminId) {
+                checkOptions(inventory);
+                res.render('inventory/update', { // call views/inventory/editInventory.handlebar to render the edit inventory page
+                    inventory
+                });
+            } else {
+                alertMessage(res, 'danger', 'Unauthorised access to Inventory', 'fas fa-exclamation-circle', true);
+                res.redirect('/logout');
+            }
+        }
+    }).catch(err => console.log(err)); // To catch no inventory ID
+});
+
+router.put('/update/:id', (req, res) => {
+    let item_name = req.body.item_name;
+    let supplier = req.body.supplier;
+    let product_name = req.body.product_name;
+    let quantity = req.body.quantity;
+
+    Inventory.update({
+        item_name,
+        supplier,
+        product_name,
+        quantity
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(() => {
+        res.redirect('/inventory/view'); // redirect to call router.get(/listInventory...) to retrieve all updated
+        // inventory
+    }).catch(err => console.log(err));
+});
+
+router.get('/delete/:id', (req, res) => {
+    let inventoryId = req.params.id;
+    let adminId = req.admin.id;
+    // Select * from inventory where inventory.id=inventoryID and inventory.adminId=adminID
+    Inventory.findOne({
+        where: {
+            id: inventoryId,
+            adminId: adminId
+        },
+        attributes: ['id', 'adminId']
+    }).then((inventory) => {
+        // if record is found, admin is owner of inventory
+        if (inventory != null) {
+            Inventory.destroy({
+                where: {
+                    id: inventoryId
+                }
+            }).then(() => {
+                alertMessage(res, 'info', 'Inventory deleted', 'far fa-trash-alt', true);
+                res.redirect('/inventory/view'); // To retrieve all inventory again
+            }).catch(err => console.log(err));
+        } else {
+            alertMessage(res, 'danger', 'Unauthorised access to Inventory', 'fas fa-exclamation-circle', true);
+            res.redirect('/logout');
+        }
     });
 });
 
--- routes/order.js
+++ routes/order.js
@@ -1,23 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const Order = require('../models/Order');
+const alertMessage = require('../helpers/messenger.js');
 
-router.get('/track', (req, res) => {
-    const title = 'Order';
+// router.get('/view', (req, res) => {
+//     const title = 'Order';
 
-    res.render('order/view', {
-        layout: "admin",
-        title: title
-    });
+//     res.render('order/view', {
+//         layout: "admin",
+//         title: title
+//     });
 
+// });
+
+router.get('/view', (req, res) => {
+    Order.findAll({
+        where: {
+            adminId: req.admin.id
+        },
+        order: [
+            ['item_name', 'ASC']
+        ],
+        raw: true
+    })
+        .then((order) => {
+            // pass object to listOrder.handlebar
+            res.render('order/view', {
+                order: order
+            });
+        })
+        .catch(err => console.log(err));
 });
 
-router.get('/create', (req, res) => {
+router.get('/showCreate', (req, res) => {
     const title = 'Order';
 
     res.render('order/create', {
         layout: "admin",
         title: title
     });
+});
+
+router.post('/create', (req, res) => {
+    let item_name = req.body.item_name;
+    let product_name = req.body.product_name;
+    let supplier = req.body.supplier;
+    let quantity = req.body.quantity;
+    let remarks = req.body.remarks;
+
+    Order.create({
+        item_name,
+        product_name,
+        supplier,
+        quantity,
+        remarks
+    }).then((order) => {
+        res.redirect('/order/view');
+    }).catch(err => console.log(err))
 });
 
 module.exports = router;
