024447fe9a75ab511b09f2511e0e791727c24cf5
gerald <geraldkhoo10@gmail.com>
2021-06-23 16:22:41 +0800

    create reservation

--- routes/ViewReservation.js
+++
@@ -1,8 +0,0 @@
-router.get('/ViewReservation', (req, res) => {
-	const title = 'View Reservation';
-	res.render('admin/reservation/view_reservation', {
-		layout: "admin",
-		title: title
-	});
-
-});
--- routes/reservation.js
+++ routes/reservation.js
@@ -1,4 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const Supplier = require('../models/Supplier');
+const Reservation = require('../models/Reservation');
 const alertMessage = require('../helpers/messenger.js');
+
+// router.get('/viewReservation', (req, res) => {
+//     const title = 'Reservation';
+
+//     res.render('reservation/viewReservation', {
+//         layout: "admin",
+//         title: title
+//     });
+
+// });
+
+
+
+
+
+router.get('/viewReservation', (req,res) => {
+    // const title: 'Reservation';
+    Reservation.findAll({
+        where: {
+            reservationID: req.reservation.id
+        },
+        order: [
+            [reservation.id, 'ASC']
+        ],
+        raw: true
+    })
+        .then((reservation) => {
+            res.render('reservation/viewReservation', {
+                layout: "blank",
+                title: title
+            });
+        })
+        .catch(err => console.log(err));
+})
+
+module.exports = router;
