0f096481c4c61ba179a1624c702746f0a796b1dd
gerald <geraldkhoo10@gmail.com>
2021-07-04 22:19:57 +0800

    sweetalert

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Reservation = require('../models/Reservation');
 const alertMessage = require('../helpers/messenger.js');
+const Swal = require('sweetalert2');
 
 
 // User View Restaurant 
