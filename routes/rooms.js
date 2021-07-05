42bfd11cda0934b36db3b161ead296c65c4142a7
eldersquid <pristinpinky@gmail.com>
2021-07-06 01:48:01 +0800

    merge inventory and rooms

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const Supplier = require('../models/Supplier');
+const Productcat = require('../models/Productcat');
+const Inventory = require('../models/Inventory');
 
 
 router.get('/', (req, res) => {
@@ -12,10 +15,24 @@ router.get('/', (req, res) => {
 
 router.get('/apartment', (req, res) => {
 	const title = 'Apartment';
-	res.render('rooms/apartment', {
-		title: title,
-		layout : "thalia",
-	    }) // renders views/index.handlebars
+	Inventory.findAll({
+        where: {
+            // adminId: req.admin.id
+        },
+        order: [
+            ['id', 'ASC']
+        ],
+        raw: true
+    })
+        .then((inventory) => {
+            
+            res.render('rooms/apartment', {
+				title: title,
+				layout : "thalia",
+				inventory
+				})
+        })
+        .catch(err => console.log(err));
 });
 
 
