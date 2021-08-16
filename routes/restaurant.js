a8c34b9b261ab47397bbf9baa56de0e738247372
eldersquid <pristinpinky@gmail.com>
2021-08-16 23:56:05 +0800

    implemented paypal to gerald restaurant

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -7,6 +7,10 @@ const Contact = require('../models/Contact');
 const FoodCart = require('../models/FoodCart');
 const Response = require('../models/Response');
 const FoodGallery = require('../models/FoodGallery');
+const paypal = require('paypal-rest-sdk');
+const app = express();
+const cors = require('cors');
+const { Template } = require('ejs');
 
 // User View Restaurant 
 // router.get('/DineV2', (req, res) => {
@@ -17,9 +21,14 @@ const FoodGallery = require('../models/FoodGallery');
 
 // User View Menu
 
+
+
 router.get("/Menu", (req, res) => {
     const title = "Menu";
-  
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     FoodCart.findAll({
       order: [["id", "ASC"]],
       raw: true,
@@ -34,6 +43,153 @@ router.get("/Menu", (req, res) => {
       })
       .catch((err) => console.log(err));
   });
+
+  router.post('/payPal', (req,res) => {
+	let totalPrice = req.body.totalPrice;
+    console.log(totalPrice);
+			const create_payment_json = {
+				"intent": "sale",
+				"payer": {
+					"payment_method": "paypal"
+				},
+				"redirect_urls": {
+					"return_url": "http://localhost:5000/restaurant/paySuccess/" + totalPrice,
+					"cancel_url": "http://localhost:5000/restaurant/"
+				},
+				"transactions": [{
+					"item_list": {
+						"items": [{
+							"name": "Food Delivery",
+							"sku": totalPrice,
+							"price": totalPrice,
+							"currency": "SGD",
+							"quantity": 1
+						}]
+					},
+					"amount": {
+						"currency": "SGD",
+						"total": totalPrice
+					},
+					"description": "Hotel Booking Payment using PayPal"
+				}]
+			};
+			paypal.payment.create(create_payment_json, function (error, payment) {
+				if (error) {
+					throw error;
+				} else {
+					for (var i = 0; i < payment.links.length;i++) {
+						if (payment.links[i].rel === "approval_url"){
+							console.log("Create Payment Response");
+							console.log(payment);
+                            var payment_url = payment.links[i].href;
+                            res.end(JSON.stringify(payment_url));
+                            
+                            
+							// res.redirect(payment.links[i].href);
+							
+							
+							
+
+
+
+						}
+
+
+
+					}
+					
+					
+					
+				}
+			});
+
+
+		  
+		
+		
+	});
+
+router.get('/paySuccess/:pay', (req, res) => {
+	const title = 'Success Payment';
+    let totalPrice = req.params.pay;
+	const payerId = req.query.PayerID;
+	const paymentId = req.query.paymentId;
+		const execute_payment_json = {
+		"payer_id": payerId,
+		"transactions": [{
+			"amount": {
+				"currency": "SGD",
+				"total": totalPrice
+			}
+		}]
+	};
+
+	paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+		if (error) {
+			console.log(error.response);
+			throw error;
+		} else {
+			console.log("Get Payment Response");
+			console.log(JSON.stringify(payment));
+			res.redirect('/restaurant/success');
+		}
+	});
+
+	
+	
+
+
+
+});
+
+router.get('/success', (req, res) => {
+	const title = 'Success!';
+	res.render('restaurant/success', {
+		title: title,
+		layout : "blank",
+	    })
+});
+
+router.get('/bookingList', (req, res) => {
+	const title = "Room Booking List";
+	
+	Room.findAll({
+	
+	order: [
+	['id', 'ASC']
+	],
+	raw: true
+	})
+	.then((room) => {
+	
+	// pass object to listVideos.handlebar
+	res.render('rooms/bookingList', {
+	layout : "admin",
+	title : title,
+	room: room
+	});
+	})
+	.catch(err => console.log(err));
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
 //Create reservation 
 router.post('/createReservation', (req, res) => {
